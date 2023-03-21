4dbdf7660cd36b76e681cdf50499a6088a277424
Keiji <mikeykouw@hotmail.com>
2023-03-21 12:41:48 +0100

    ###

--- site/index.php
+++ site/index.php
@@ -1,5 +1,32 @@
 <?php
-
 // Dit is het startpunt van je applicatie.
+?>
 
-echo 'Welkom bij het receptenboek';
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\style.css">
+    <title>Webshop</title>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <div class="row"> 
+                    <ul>
+                        <li><a href=""><img src='../images/recepten.png'></a></li>
+                        <li><a href=""><img src=""></a></li>
+                        <li><a href=""><img src=""></a></li>
+                    </ul>
+            </div> <!--Einde Row -->
+            <div class="line"></div>
+        </div> <!--Einde Container-->
+    </header>
+</body>
+
+</html>
