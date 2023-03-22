b430f3ab37b837b8c0e12b206128fd049a20f0c4
Keiji <mikeykouw@hotmail.com>
2023-03-22 08:55:43 +0100

    ##

--- site/index.php
+++ site/index.php
@@ -11,19 +11,39 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css\style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Alumni+Sans:wght@200&display=swap" rel="stylesheet">
     <title>Webshop</title>
 </head>
 
 <body>
     <header>
         <div class="container">
-            <ul>
-                <li><a href=""><img src='../images/recepten.png' class="icons-menu"></a></li>
-                <li><a href=""><img src='../images/recepten.png' class="icons-menu"></a></li>
-                <li><a href=""><img src='../images/recepten.png' class="icons-menu"></a></li>
-            </ul>
+            <nav>
+                <ul>
+                    <li><a href=""><img src='../images/recepten.png' class="icons-menu"></a></li>
+                    <li><a href=""><img src='/images/reserveren.png' class="icons-menu"></a></li>
+                    <li><a href="#main-menu"><img src='/images/menu.png' class="icons-menu"></a></li>
+                </ul>
+            </nav>
             <div class="line"></div>
+
+            <main>
+                <div class="row">
+                    <div class="catagorie-restaurant">
+                        <li>Lounge |</li>
+                        <li>Restaurant</li>
+                    </div>
+                    <div class="text-main">
+                    <h1>The best food & drinks in a refined but relaxed setting.</h1>
+                    </div>
+                    <a href="#" class="btn btn4">Reserveren</a>
+                    <a href="#" class="btn btn4">Bekijk het menu</a>
+                </div><!--Einde Row-->
         </div> <!--Einde Container-->
+        </main>
+
     </header>
 </body>
 
